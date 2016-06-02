cccee9269e73ebe303cf3d270fa528f076aa3159
shari-singh <shari.singh93@gmail.com>
2016-06-02 12:01:17 -0400

    added parent class and fixed name errors

--- people.rb
+++ people.rb
@@ -1,27 +1,29 @@
-class Student
+class Person
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
+  def greeting
+    puts "Hi my name is #{name}"
+  end
+end
+
+class Student < Person
   def learn
     puts "I get it!"
   end
-  def name(name)
-    @name = name
-    puts "Hi my name is #{name}"
-  end
 end
 
-class Instructor
+class Instructor < Person
   def teach
     puts "Everything in Ruby is an Object"
   end
-  def name(name)
-    @name = name
-    puts "Hi my name is #{name}"
-  end
 end
 
-chris = Instructor.new
-puts chris.name("Chris")
-cristina = Student.new
-puts cristina.name("Cristina")
+chris = Instructor.new("Chris")
+cristina = Student.new("Cristina")
+puts chris.name
+puts cristina.name
 puts chris.teach
 puts cristina.learn
-# puts student.teach   teach is a method in Instructor. Not defined in Student and cannot be accessed by Student
+# puts student.teach   teach is a method in Instructor. Not defined in Student and cannot be accessed by
