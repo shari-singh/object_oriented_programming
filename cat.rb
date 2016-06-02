51c2fed10317695703aca4e469d3dbee7b8abd98
shari-singh <shari.singh93@gmail.com>
2016-06-02 13:17:47 -0400

    created Cat class

---
+++ cat.rb
@@ -0,0 +1,8 @@
+class Cat
+attr_reader :name, :preferred_food, :meal_time
+  def initialize (name, preferred_food, meal_time)
+    @name = name
+    @preferred_food = preferred_food
+    @meal_time = meal_time
+  end
+end
